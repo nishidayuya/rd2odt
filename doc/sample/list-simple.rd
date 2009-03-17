6b680d2de8d843c770107162d68ed28440384a10
Yuya.Nishida <yuya@j96.org>
2009-03-17 16:23:35 +0900

    added test.

---
+++ doc/sample/list-simple.rd
@@ -0,0 +1,7 @@
+=begin
+
+(1) EnumList 1
+* ItemList 1
+(1) EnumList 1
+
+=end
--- test/functional/rd2odt-spec.rb
+++ test/functional/rd2odt-spec.rb
@@ -101,6 +101,45 @@ describe RD2ODT::RD2ODTVisitor, "" do
     @visitor = RD2ODT::RD2ODTVisitor.new
   end
 
+  it "supports the EnumList over ItemList." do
+    result = @visitor.visit(create_rd_tree("list-simple.rd"))
+    check_document_content(result) do |office_text|
+      office_text[0].should == [:text__list,
+                                {
+                                  :text__continue_numbering => "true",
+                                  :text__style_name => "Numbering_20_1",
+                                },
+                                [:text__list_item,
+                                 [:text__p,
+                                  {:text__style_name => "Text_20_body"},
+                                  "EnumList 1",
+                                 ],
+                                ]
+                               ]
+      office_text[1].should == [:text__list,
+                                {:text__style_name => "List_20_1"},
+                                [:text__list_item,
+                                 [:text__p,
+                                  {:text__style_name => "Text_20_body"},
+                                  "ItemList 1",
+                                 ],
+                                ]
+                               ]
+      office_text[2].should == [:text__list,
+                                {
+                                  :text__continue_numbering => "true",
+                                  :text__style_name => "Numbering_20_1",
+                                },
+                                [:text__list_item,
+                                 [:text__p,
+                                  {:text__style_name => "Text_20_body"},
+                                  "EnumList 1",
+                                 ],
+                                ]
+                               ]
+    end
+  end
+
   it "process multi-paragraph.rd" do
     result = @visitor.visit(create_rd_tree("multi-paragraph.rd"))
     check_document_content(result) do |office_text|
